12c671d9fca48fb74ab3f4957043cdf4a41b1763
Nathan Rothenberger <nroth10@gmail.com>
2024-07-30 18:17:18 -0500

    fix sans-font

--- apps/demo/src/pages/_app.tsx
+++ apps/demo/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div id="__app_root" className={cn(fontSans.variable)}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
