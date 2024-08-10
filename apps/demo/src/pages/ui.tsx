6f9729b43a9b58215ececc83fea6c91bb81d710e
Nathan Rothenberger <nroth10@gmail.com>
2024-08-10 16:37:21 -0500

    stub out ui route/page

---
+++ apps/demo/src/pages/ui.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import * as Text from "@/components/ui/text";
+import { MockBreadcrumb } from "@/components/ui/mock";
+import { Layout } from "@/components/ui/layout";
+
+export default function Ui() {
+  // tab state
+  return (
+    <Layout>
+      <div className="flex flex-col gap-4">
+        <Text.Header>Button</Text.Header>
+        <Text.Header>Size</Text.Header>
+        <div className="flex flex-col gap-4 items-start">
+          <Button variant="default" size="icon">
+            Button
+          </Button>
+          <Button variant="default" size="sm">
+            Button
+          </Button>
+          <Button variant="default" size="default">
+            Button
+          </Button>
+          <Button variant="default" size="lg">
+            Button
+          </Button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
