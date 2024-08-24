f491d5268ad65f5e58246c948a3496c9bb7a2a3e
Nathan Rothenberger <nroth10@gmail.com>
2024-08-24 13:11:00 -0500

    chore: add fonts to root of app (for portals)

--- apps/demo/src/pages/_app.tsx
+++ apps/demo/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import { useEffect } from "react";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -11,8 +12,16 @@ const fontSans = FontSans({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Add this to the body so that it works on portaled components
+  useEffect(() => {
+    document.body.classList.add(fontSans.variable);
+    return () => {
+      document.body.classList.remove(fontSans.variable);
+    };
+  }, []);
+
   return (
-    <div id="__app_root" className={cn(fontSans.variable)}>
+    <div id="__app_root">
       <Component {...pageProps} />
     </div>
   );
