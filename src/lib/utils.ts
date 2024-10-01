7d3c02f6486ef6d3c701799b5c3bece39d95daa9
Joel Turner <joel@joelmturner.com>
2024-10-01 13:49:48 -0700

    initialize vitest and CI runner
    ""

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date) {
+  return format(date, "MM/dd/yyyy");
 }
