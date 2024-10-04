b5d404ac58f56318966bd07daf5348df12fa0b5e
Ryan Legler <ryan.legler@gmail.com>
2024-10-04 14:09:56 -0700

    chore: ignore public / remove react version detect

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -8,6 +8,8 @@ module.exports = {
     "eslint:recommended",
     require.resolve("@vercel/style-guide/eslint/next"),
     "turbo",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
   globals: {
     React: true,
@@ -25,53 +27,11 @@ module.exports = {
       },
     },
   },
-  react: {
-    version: "detect", // Automatically detect the react version
-  },
   ignorePatterns: [
     // Ignore dotfiles
     ".*.js",
+    "**/public/",
     "node_modules/",
   ],
   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
 };
-
-// const { resolve } = require("node:path");
-
-// const project = resolve(process.cwd(), "tsconfig.json");
-
-// /** @type {import("eslint").Linter.Config} */
-// module.exports = {
-//   extends: [
-//     "eslint:recommended",
-//     require.resolve("@vercel/style-guide/eslint/next"),
-//     "turbo",
-//     "plugin:react/recommended",
-//     "plugin:react-hooks/recommended",
-//   ],
-//   globals: {
-//     React: true,
-//     JSX: true,
-//   },
-//   env: {
-//     node: true,
-//     browser: true,
-//   },
-//   plugins: ["only-warn"],
-//   settings: {
-//     "import/resolver": {
-//       typescript: {
-//         project,
-//       },
-//     },
-// react: {
-//   version: "detect", // Automatically detect the react version
-// },
-//   },
-//   ignorePatterns: [
-//     // Ignore dotfiles
-//     ".*.js",
-//     "node_modules/",
-//   ],
-//   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-// };
