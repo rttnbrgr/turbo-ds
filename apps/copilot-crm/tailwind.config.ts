be9892b21644b2720f82e409b9de3728aaaccdba
Ryan Legler <ryan.legler@gmail.com>
2024-10-05 13:11:25 -0700

    chore: demonstrate how to customize UI tokens per app

--- apps/copilot-crm/tailwind.config.ts
+++ apps/copilot-crm/tailwind.config.ts
@@ -2,16 +2,16 @@ import baseConfig from "@repo/ui/tailwind.config";
 
 const overrideConfig = {
   presets: [baseConfig],
-  // theme: {
-  //   extend: {
-  //     colors: {
-  //       custom: "magenta",
-  //       primary: {
-  //         foreground: "green",
-  //       },
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {
+      colors: {
+        custom: "var(--custom)",
+        // primary: {
+        //   foreground: "green",
+        // },
+      },
+    },
+  },
 };
 
 export default overrideConfig;
