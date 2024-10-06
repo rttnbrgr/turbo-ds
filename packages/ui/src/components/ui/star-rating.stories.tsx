eed921f1839cf35317d5da0017a1efc8f0a09f03
Ryan Legler <ryan.legler@gmail.com>
2024-10-06 08:29:10 -0700

    docs: adds toggle-group stories

--- packages/ui/src/components/ui/star-rating.stories.tsx
+++ packages/ui/src/components/ui/star-rating.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Meta, StoryObj } from "@storybook/react";
 import { StarRating } from "@repo/ui/components/ui/star-rating";
 
